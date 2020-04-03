86a97e8349a196021315a7a2cfa6550e026eb68c
paul623 <zhubaoluo@outlook.com>
2020-04-03 10:08:42 +0800

    更新了JDK8的计算日期方法

--- app/src/main/java/com/paul/himynote/Tools/DateUtils.java
+++ app/src/main/java/com/paul/himynote/Tools/DateUtils.java
@@ -1,8 +1,15 @@
 package com.paul.himynote.Tools;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import com.paul.himynote.Model.NoteBean;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -16,17 +23,24 @@ public class DateUtils {
     /**
      * 计算两个日期之间的差值
      * B-A
+     * 更新JDK8的计算方法，更准确兼容性更好
+     * 显然这个方法都不适合其他时区···
      * */
-    public static long countDayToInt(String dateA,String dateB){
-        Calendar a = Calendar.getInstance(),
-                b = Calendar.getInstance();
-        String[] splitdate = dateA.split("-");
-        a.set(Integer.parseInt(splitdate[0]), Integer.parseInt(splitdate[1]) - 1, Integer.parseInt(splitdate[2]));
-        String[] splitdateB = dateB.split("-");
-        b.set(Integer.parseInt(splitdateB[0]), Integer.parseInt(splitdateB[1]) - 1, Integer.parseInt(splitdateB[2]));
-        long diffDays = (b.getTimeInMillis() - a.getTimeInMillis())
-                / (1000 * 60 * 60 * 24);
-        return diffDays;
+    public static long countDayToInt(String dateA, String dateB){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            return ChronoUnit.DAYS.between(LocalDate.parse(dateA),LocalDate.parse(dateB));
+
+        }else {
+            String[] splitdate = dateA.split("-");
+            String[] splitdateB = dateB.split("-");
+            Calendar a = Calendar.getInstance(),
+                    b = Calendar.getInstance();
+            a.set(Integer.parseInt(splitdate[0]), Integer.parseInt(splitdate[1]) - 1, Integer.parseInt(splitdate[2]));
+            b.set(Integer.parseInt(splitdateB[0]), Integer.parseInt(splitdateB[1]) - 1, Integer.parseInt(splitdateB[2]));
+            return (b.getTimeInMillis() - a.getTimeInMillis())
+                    / (1000 * 60 * 60 * 24);
+        }
+
     }
     public static String getCountDay(NoteBean noteBean){
         int min=(int)countDayToInt(getCurDate(),noteBean.getEndDate());
